53044c00e8fe7d27daed29832cc2154e2fa892e9
Mayonnaise-Slap <368758@niuitmo.ru>
2023-09-23 15:19:47 +0300

    Storing config in a separate file

---
+++ .config.py
@@ -0,0 +1,7 @@
+class config:
+    def __init__(self):
+        self.database = "database"
+        self.user = "postgres"
+        self.password = "password"
+        self.host = "localhost"
+        self.port = "5432"
