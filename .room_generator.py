98d55829411ae2379c0ae21f04b2c4791dd08c41
Mayonnaise-Slap <368758@niuitmo.ru>
2023-09-23 15:20:13 +0300

    fixed room generation

--- .room_generator.py
+++ .room_generator.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
         campus = json.load(inp)
 
     rooms = {}
-    for id in campus:
+    for campus_id in campus:
         for floor in range(1, 5):
-            for id in range(1, random.randint(1, 5) * 5):
-                room = f"{random.randint(1, 4)}{floor}{str(id).rjust(2, '0')}"
-                rooms[room] = {"campus": campus,
+            for rooms_on_floor in range(1, random.randint(1, 5) * 5):
+                room = f"{random.randint(1, 4)}{floor}{str(rooms_on_floor).rjust(2, '0')}"
+                rooms[room] = {"campus": campus_id,
                                "space": random.randint(3, 12)*5}
     with open(f"generated/rooms.json", "w") as out:
         json.dump(rooms, out)
